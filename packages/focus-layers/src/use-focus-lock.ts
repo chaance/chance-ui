c33f261c82af145fe2610c027b9dfa643d3c5edb
Chance Strickland <hi@chancedigital.io>
2021-01-02 20:20:50 -0800

    rm use-focus-return module

--- packages/focus-layers/src/use-focus-lock.ts
+++ packages/focus-layers/src/use-focus-lock.ts
@@ -11,79 +11,18 @@
  */
 
 import * as React from "react";
-import LockStack, { LockListener } from "./lock-stack";
-
-// This global ensures that only one stack exists in the document. Having
-// multiple active stacks does not make sense, as documents are only capable of
-// having one activeElement at a time.
-export const LOCK_STACK = new LockStack();
-
-let lockCount = 0;
-function newLockUID() {
-	return `lock-${lockCount++}`;
-}
-
-/**
- * Creates a subscription to the lock stack that is bound to the lifetime of the
- * caller, based on `useEffect`.
- */
-export function useLockSubscription(callback: LockListener) {
-	// `subscribe` returns an `unsubscribe` function that `useEffect` can invoke
-	// to clean up the subscription.
-	React.useEffect(() => LOCK_STACK.subscribe(callback), [callback]);
-}
-
-/**
- * Set up a return of focus to the target element specified by `returnTo` to
- * occur at the end of the lifetime of the caller component. In other words,
- * return focus to where it was before the caller component was mounted.
- */
-export function useFocusReturn(disabledRef?: React.RefObject<boolean>) {
-	// This isn't necessarily safe, but realistically it's sufficient.
-	const [target] = React.useState(() => document?.activeElement as HTMLElement);
-
-	React.useLayoutEffect(() => {
-		return () => {
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-			if (disabledRef != null && disabledRef.current) {
-				return;
-			}
-			// Happens on next tick to ensure it is not overwritten by focus lock.
-			requestAnimationFrame(() => {
-				target != null && target.focus();
-			});
-		};
-		// Explicitly only want this to run on unmount
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, []);
-}
-
-/**
- * Create and push a new lock onto the global LOCK_STACK, tied to the lifetime
- * of the caller. Returns a ref containing the current enabled state of the
- * layer, to be used for enabling/disabling the caller's lock logic.
- */
-export function useLockLayer(controlledUID?: string) {
-	const [uid] = React.useState(() => controlledUID || newLockUID());
-	const enabledRef = React.useRef(false);
-
-	React.useLayoutEffect(() => {
-		LOCK_STACK.add(uid, (enabled) => (enabledRef.current = enabled));
-		return () => LOCK_STACK.remove(uid);
-	}, [uid]);
-
-	return enabledRef;
-}
+import { useLockLayer } from "./use-lock-layer";
+import { wrapFocus } from "./utils";
 
 export default function useFocusLock(
 	containerRef: React.RefObject<HTMLElement>,
 	options: {
-		disableReturnRef?: React.RefObject<boolean>;
+		disableReturn?: boolean;
 		attachTo?: HTMLElement | Document;
 		disable?: boolean;
 	} = {}
 ) {
-	const { disableReturnRef, attachTo = document, disable } = options;
+	const { disableReturn, attachTo = document, disable } = options;
 	// Create a new layer for this lock to occupy
 	const enabledRef = useLockLayer();
 
@@ -166,44 +105,29 @@ export default function useFocusLock(
 	// This happens at the end to absolutely ensure that the return is the last
 	// thing that will run as part of this hook (i.e., that the focus handlers
 	// have been fully detached).
-	useFocusReturn(disableReturnRef);
-}
-
-function createFocusWalker(root: HTMLElement) {
-	return document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, {
-		acceptNode: (node: HTMLElement) =>
-			// `.tabIndex` is not the same as the `tabindex` attribute. It works on
-			// the runtime's understanding of tabbability, so this automatically
-			// accounts for any kind of element that could be tabbed to.
-			node.tabIndex >= 0 && !(node as any).disabled
-				? NodeFilter.FILTER_ACCEPT
-				: NodeFilter.FILTER_SKIP,
-	});
+	useFocusReturn(disableReturn);
 }
 
 /**
- * Given a `root` container element and a `target` that is outside of that
- * container and intended to receive focus, force the DOM focus to wrap around
- * such that it remains within `root`.
- *
- * If `forceFirst` is set to `true`, the wrap will always attempt to focus the
- * first viable element in `root`, rather than wrapping to the end.
+ * Return focus to where it was before the caller component was mounted.
  */
-function wrapFocus(
-	root: HTMLElement,
-	target: Element,
-	forceFirst: boolean = false
-) {
-	const walker = createFocusWalker(root);
-	const position = target.compareDocumentPosition(root);
-	let wrappedTarget: HTMLElement | null = null;
+function useFocusReturn(disabled?: boolean) {
+	// This isn't necessarily safe, but realistically it's sufficient.
+	const [target] = React.useState(() => document?.activeElement as HTMLElement);
 
-	if (position & Node.DOCUMENT_POSITION_PRECEDING || forceFirst) {
-		wrappedTarget = walker.firstChild() as HTMLElement | null;
-	} else if (position & Node.DOCUMENT_POSITION_FOLLOWING) {
-		wrappedTarget = walker.lastChild() as HTMLElement | null;
-	}
+	React.useLayoutEffect(() => {
+		return () => {
+			// eslint-disable-next-line react-hooks/exhaustive-deps
+			if (disabled) {
+				return;
+			}
 
-	const newFocus = wrappedTarget != null ? wrappedTarget : root;
-	newFocus.focus();
+			// Happens on next tick to ensure it is not overwritten by focus lock.
+			requestAnimationFrame(() => {
+				target != null && target.focus();
+			});
+		};
+		// Explicitly only want this to run on unmount
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
 }
