9c1f9789ed5f0cfcbce0f553a0d143056dad52fa
Chance Strickland <hi@chance.dev>
2022-04-24 13:56:36 -0700

    add scripts and tsconfig

---
+++ scripts/copy-license.mjs
@@ -0,0 +1,30 @@
+import * as path from "path";
+import * as fsp from "fs/promises";
+import { performance } from "perf_hooks";
+
+const rootDir = process.cwd();
+const packagesDir = path.join(rootDir, "packages");
+
+main();
+
+async function main() {
+	let start = performance.now();
+	let [packages, licenseContents] = await Promise.all([
+		fsp.readdir(packagesDir),
+		fsp.readFile(path.join(rootDir, "LICENSE"), "utf8"),
+	]);
+	if (!licenseContents) {
+		throw new Error("No license file found");
+	}
+	for (let packageName of packages) {
+		let packageDir = path.join(packagesDir, packageName);
+		let licenseFilePath = path.join(packageDir, "LICENSE");
+		await fsp.writeFile(licenseFilePath, licenseContents);
+	}
+
+	console.log(
+		`✨ Copied LICENSE to all packages in ${
+			Math.round((performance.now() - start) * 100) / 100
+		}ms`
+	);
+}
