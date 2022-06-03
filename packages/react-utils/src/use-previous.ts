017a28304781d0642675befc094fce9a5fbaa5ea
Chance Strickland <hi@chance.dev>
2022-06-03 11:12:56 -0400

    fix typo

--- packages/react-utils/src/use-previous.ts
+++ packages/react-utils/src/use-previous.ts
@@ -6,7 +6,7 @@ import { useRef, useEffect } from "react";
  * @param value
  */
 export function usePrevious<ValueType = any>(value: ValueType) {
-	console.log("usePrevious is unsafe, probably");
+	console.log("usePrevious is unsafe, probably!");
 	const ref = useRef<ValueType | null>(null);
 	useEffect(() => {
 		ref.current = value;
